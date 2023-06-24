61bdd190e8b8bc218cbc8ae3bcf31a5d418ac38b
edwintantawi <edwintantawi.workspace@gmail.com>
2023-06-25 01:58:41 +0700

    feat: add google fonts
    - Inter as font sans
    - JetBrains Mono as font mono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 
+import { fontMono, fontSans } from '~/common/fonts';
+import { cn } from '~/common/utils';
+
 import './style.css';
 
 interface RootLayoutProps {
@@ -9,7 +12,9 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={cn('font-sans', fontSans.variable, fontMono.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
