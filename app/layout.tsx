92f2412cc865fc62b9d311c563b28416683ea289
edwintantawi <edwintantawi.workspace@gmail.com>
2023-06-29 12:53:30 +0700

    feat: add metadata with icons and manifest
    - add link to manifest file
    - add apple-touch-icon
    - add favicon .ico
    - add favicon .svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,20 @@ import { cn } from '~/common/utils';
 
 import './style.css';
 
+export const metadata = {
+  title: 'Shopnify',
+  description: 'An open source web-based marketplace application',
+  manifest: '/manifest.json',
+  icons: {
+    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon.ico', sizes: '16x16' },
+      { url: '/favicon.ico', sizes: '32x32' },
+      { url: '/icon.svg', type: 'image/svg+xml' },
+    ],
+  },
+};
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
